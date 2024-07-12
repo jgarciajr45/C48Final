f1fe5f71ca425b56d7349a286ffef4c8f0c9043d
Jesus Garcia <jgarciajr45@gmail.com>
2024-07-12 09:35:29 -0700

    I can see product

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+
+public interface IProductRepository
+{
+    IEnumerable<Product> GetAllProducts();
+}
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,20 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySql.Data.MySqlClient;
+using Testing.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+// Register the database connection service
+builder.Services.AddScoped<IDbConnection>((sp) =>
+    new MySqlConnection(builder.Configuration.GetConnectionString("bestbuy")));
 
+// Register the repository service
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
